177bf5ad9d8e2c246cf616c9fa305fe8c816dd2b
aronlg <aronlgreen@gmail.com>
2022-03-24 01:22:30 +0000

    Redirect to /trails for now - not using frontpage atm

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/trails',
+        permanent: false,
+      },
+    ]
+  },
   env: {
     MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
   },
