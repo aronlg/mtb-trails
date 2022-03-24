acc9687101f8d6254be51ed769b9aac351c1fb80
aronlg <aronlgreen@gmail.com>
2022-03-24 01:11:16 +0000

    Base setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react'
+import '../styles/globals.css'
+import theme from '../theme'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
