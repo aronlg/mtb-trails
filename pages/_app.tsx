babd98eb9de6e83c0b3df3e8a70aeda8c8cd7220
aronlg <aronlgreen@gmail.com>
2022-03-27 21:52:56 +0000

    Create single Trail page (slug param)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
+import { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
-import '../styles/globals.css'
-import theme from '../theme'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import theme from '../theme'
+import MapLayout from '../layouts/MapLayout'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
+  if (router.pathname.startsWith('/trails')) {
+    return (
+      <NextUIProvider theme={theme}>
+        <MapLayout>
+          <Component {...pageProps} />
+        </MapLayout>
+      </NextUIProvider>
+    )
+  }
+
   return (
     <NextUIProvider theme={theme}>
       <Component {...pageProps} />
---
+++ pages/trails/[slug].tsx
@@ -0,0 +1,52 @@
+import type { NextPage, GetStaticProps, GetStaticPaths } from 'next'
+import { Text, Spacer } from '@nextui-org/react'
+import { TrailsType, TrailType } from '../../types'
+import Elevation from '../../components/Elevation'
+
+interface Props {
+  trails: TrailsType
+  trail: TrailType
+}
+
+const TrailPage: NextPage<Props> = ({ trail }: Props) => {
+  const { description, geoJson, gpxGeoJson, distance, rating } = trail
+  // @ts-ignore
+  const { name } = geoJson.features[0].properties
+
+  return (
+    <>
+      <Text h1>{name}</Text>
+      <Text size={20}>{description}</Text>
+      <Spacer y={2} />
+      <Elevation coordinates={gpxGeoJson.features[0].geometry.coordinates} />
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const gpxUtils = require('../../utils/gpx')
+
+  const paths = gpxUtils.trails.map((trail: TrailType) => ({
+    params: { slug: trail.slug },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const gpxUtils = require('../../utils/gpx')
+
+  const { trails } = gpxUtils
+  const trail = trails.filter((trail: TrailType) => trail.slug === params?.slug)
+
+  return {
+    props: {
+      trail: trail[0],
+    },
+  }
+}
+
+export default TrailPage
