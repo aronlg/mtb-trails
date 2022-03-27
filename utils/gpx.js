6db170584bdce80e06eafd7c4a26462de8691f54
aronlg <aronlgreen@gmail.com>
2022-03-27 21:44:23 +0000

    Update gpx

--- utils/gpx.js
+++ utils/gpx.js
@@ -5,31 +5,29 @@ const turflength = require('@turf/length').default
 const xmldom = require('xmldom')
 const data = require('../data/trails')
 
-const ROUTES_PATH = path.join(process.cwd(), 'public', 'gpx')
+const TRAILS_PATH = path.join(process.cwd(), 'public', 'gpx')
 
-// routeFilePaths is the list of all gpx files inside the ROUTES_PATH directory
-const routeFilePaths = fs
-  .readdirSync(ROUTES_PATH)
-  // Only include gpx files
+const trailFilePaths = fs
+  .readdirSync(TRAILS_PATH)
   .filter((p) => /\.gpx?$/.test(p))
 
-const routes = routeFilePaths.map((filePath) => {
+const trails = trailFilePaths.map((filePath) => {
   const source = new xmldom.DOMParser().parseFromString(
-    fs.readFileSync(path.join(ROUTES_PATH, filePath), 'utf8')
+    fs.readFileSync(path.join(TRAILS_PATH, filePath), 'utf8')
   )
   const slug = filePath.replace('.gpx', '')
   const trail = data.trails[slug]
 
   const geoJson = toGeoJson.gpx(source)
 
-  // Calculate distance using geoJson
+  // Distance
   const distance = turflength(geoJson)
 
-  // Calculate elevation gain using gpx data
+  // Elevation
   const { coordinates } = geoJson.features[0].geometry
   let elevation = 0
   coordinates.forEach((coord, index) => {
-    if (index === coordinates.length - 1) return // stop 1 point early since comparison requires 2 points
+    if (index === coordinates.length - 1) return
     const elevationDifference =
       coordinates[index + 1][2] - coordinates[index][2]
     if (elevationDifference > 0) elevation += elevationDifference
@@ -39,13 +37,12 @@ const routes = routeFilePaths.map((filePath) => {
     distance,
     elevation,
     geoJson: trail?.geoJson || geoJson,
-    gpxGeoJson: geoJson, // TODO send original gpx geojson to generate-images until it handles the multi-feature geojson
+    gpxGeoJson: geoJson,
     slug,
-    color: trail?.color || 'red',
     description: trail?.description || null,
     rating: trail?.rating || null,
     location: trail?.location || null,
   }
 })
 
-module.exports = { routes }
+module.exports = { trails }
