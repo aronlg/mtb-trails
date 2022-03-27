dd4a395166fb4aef1c518ed08fac52a0383b1f3e
aronlg <aronlgreen@gmail.com>
2022-03-27 21:45:45 +0000

    Update/edit types

--- types/index.ts
+++ types/index.ts
@@ -2,15 +2,22 @@ import { GeoJSON } from './geojson'
 
 export type Rating = 1 | 2 | 3 | 4 | 5
 
-export interface Trail {
+export interface CordsType {
+  lat: number
+  lon: number
+  elevation: number
+}
+
+export interface TrailType {
   slug: string
   distance: number
   elevation: number
   geoJson: GeoJSON.FeatureCollection
+  gpxGeoJson: GeoJSON.FeatureCollection
   rating?: Rating
   description?: string
   location?: string
   color?: string
 }
 
-export type Trails = Array<Trail>
+export type TrailsType = Array<TrailType>
