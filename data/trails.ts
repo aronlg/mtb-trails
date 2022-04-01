166033b981e71ba74600e9c268fe355c4d113882
aronlg <aronlgreen@gmail.com>
2022-04-01 18:46:43 +0000

    Add new gpx trail: Laugavegur

--- data/trails.ts
+++ data/trails.ts
@@ -10,23 +10,31 @@ interface TrailsJSON {
 
 const trails: TrailsJSON = {
   hvaleyrarvatn: {
-    description: 'Stuttur hringur í Hafnarfirði',
+    description: 'Stuttur hringur í Hafnarfirði.',
     rating: 1,
     location: 'Hafnarfjörður',
   },
   storhofdi: {
-    description: 'Skemmtilegt svæði kringum Hvaleyrarvatn',
+    description: 'Skemmtilegt svæði kringum Hvaleyrarvatn.',
     rating: 2,
     location: 'Hafnarfjörður',
   },
   jadarinn: {
-    description: 'Frá Blafjöllum að Heiðmörk',
+    description: 'Frá Blafjöllum að Heiðmörk.',
     rating: 3,
     location: 'Hafnarfjörður',
   },
   helgafell: {
+    description:
+      'Klifraðu í gegnum helvíti fyrir frábæra upplifun á niðurleið.',
     rating: 4,
     location: 'Hafnarfjörður',
+  },
+  laugavegur: {
+    description:
+      'Landmannalaugar til Þórsmerkur, í gegnum Hrafntinnusker, Álftavatn og Emstrur.',
+    rating: 4,
+    location: 'Landmannalaugar',
   },
 }
 
